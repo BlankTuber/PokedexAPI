d6292aea527f3929765015c51f46c97e18261891
Mumeisho <blank@quidque.no>
2025-09-08 12:56:28 +0200

    Added a static pokemon

--- pokepi/src/main.rs
+++ pokepi/src/main.rs
@@ -13,7 +13,21 @@ fn hello(name: &str) -> Result<Json<Value>, Status> {
     })))
 }
 
+#[get("/pokemon")]
+fn pokemon() -> Result<Json<Value>, Status> {
+    Ok(Json(json!({
+      "pokemon": {
+        "name": "Eevee",
+        "type": "Normal",
+        "abilities": ["Run Away", "Adaptability"],
+        "gender": 0.5,
+        "height": 1.0,
+        "weight": 14.3
+      }
+    })))
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build().mount("/", routes![hello, pokemon])
 }
