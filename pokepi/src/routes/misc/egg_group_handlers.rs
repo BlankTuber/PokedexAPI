ff205d534c8f52ffdbcd241fcba8655dfdd75ee6
Mumeisho <blank@quidque.no>
2025-09-18 10:35:55 +0200

    Added an update route

--- pokepi/src/routes/misc/egg_group_handlers.rs
+++ pokepi/src/routes/misc/egg_group_handlers.rs
@@ -5,7 +5,7 @@ use rocket::{
 };
 use sqlx::PgPool;
 
-use crate::models::egg_group::CreateEggGroup;
+use crate::models::egg_group::{CreateEggGroup, UpdateEggGroup};
 
 #[post("/egg-groups", format = "json", data = "<egg_group_data>")]
 pub async fn create_egg_group(
@@ -41,4 +41,44 @@ pub async fn create_egg_group(
             "egg_group_identifier": result.egg_group_identifier
         }
     })))
+}
+
+#[patch(
+    "/egg-groups/<egg_group_id>",
+    format = "json",
+    data = "<updated_egg_group_data>"
+)]
+pub async fn update_egg_group(
+    pool: &State<PgPool>,
+    egg_group_id: i16,
+    updated_egg_group_data: Json<UpdateEggGroup>,
+) -> Result<Json<Value>, Status> {
+    let updated_egg_group = updated_egg_group_data.into_inner();
+
+    let result = sqlx::query!(
+        r#"
+        UPDATE egg_groups
+        SET egg_group_name = COALESCE($1, egg_group_name), 
+            egg_group_identifier = COALESCE($2, egg_group_identifier)
+        WHERE egg_group_id = $3
+        "#,
+        updated_egg_group.egg_group_name,
+        updated_egg_group.egg_group_identifier,
+        egg_group_id,
+    )
+    .execute(pool.inner())
+    .await;
+
+    match result {
+        Ok(query_result) => {
+            if query_result.rows_affected() == 0 {
+                Err(Status::NotFound)
+            } else {
+                Ok(Json(json!({
+                    "message": "Egg group updated successfully",
+                })))
+            }
+        }
+        Err(_) => Err(Status::InternalServerError),
+    }
 }
